c05c19fb972ee399165dffa4212625e327d891a6
Antares <67341887+antaresrvish@users.noreply.github.com>
2024-01-02 21:16:50 +0300

    test2

--- adb-fastboot-gui-app/Form1.cs
+++ adb-fastboot-gui-app/Form1.cs
@@ -9,30 +9,48 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace adb_fastboot_gui_app
 {
     public partial class Form1 : Form
     {
+        private Process process = new Process();
+        private ProcessStartInfo info = new ProcessStartInfo();
         public Form1()
         {
             InitializeComponent();
-
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.CreateNoWindow = true;
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Process process = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = false;
-            startInfo.RedirectStandardOutput = true;
-            startInfo.FileName = "adb.exe";
-            startInfo.Arguments = " adb devices";
-            process.StartInfo = startInfo;
-            process.Start();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                info.FileName = "adb.exe";
+                info.Arguments = "devices -l";
+                process.StartInfo = info;
+
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string delete = output.Substring(0, output.Length - 34);
+                string raw_device_name = delete.Substring(78);
+                string real = raw_device_name.Replace("_", " ");
+                textBox1.Text = real;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
