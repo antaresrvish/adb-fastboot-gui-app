439b04e2fc5d34e83898d23e783f0f15a6f72d08
Antares <67341887+antaresrvish@users.noreply.github.com>
2024-01-03 22:09:22 +0300

    Changes
    main menu, ram progress bar

--- adb-fastboot-gui-app/Form1.Designer.cs
+++ adb-fastboot-gui-app/Form1.Designer.cs
@@ -28,17 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel4 = new System.Windows.Forms.Panel();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.device_name_label = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.freeram_progressbar = new System.Windows.Forms.ProgressBar();
+            this.freeram_label = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -58,7 +69,7 @@
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(58)))), ((int)(((byte)(65)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Source Code Pro Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.textBox1.ForeColor = System.Drawing.Color.White;
             this.textBox1.Location = new System.Drawing.Point(13, 32);
             this.textBox1.Margin = new System.Windows.Forms.Padding(10);
@@ -84,8 +95,8 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.device_name_label);
-            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.panel1);
+            this.groupBox3.Controls.Add(this.groupBox4);
             this.groupBox3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox3.ForeColor = System.Drawing.Color.White;
             this.groupBox3.Location = new System.Drawing.Point(506, 11);
@@ -97,28 +108,20 @@
             // 
             // device_name_label
             // 
-            this.device_name_label.AutoSize = true;
+            this.device_name_label.Dock = System.Windows.Forms.DockStyle.Fill;
             this.device_name_label.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.device_name_label.ForeColor = System.Drawing.Color.White;
-            this.device_name_label.Location = new System.Drawing.Point(108, 25);
+            this.device_name_label.Location = new System.Drawing.Point(0, 0);
             this.device_name_label.Name = "device_name_label";
-            this.device_name_label.Size = new System.Drawing.Size(49, 15);
+            this.device_name_label.Size = new System.Drawing.Size(167, 28);
             this.device_name_label.TabIndex = 6;
             this.device_name_label.Text = "DEVICE";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(9, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 21);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "DEVICE :";
+            this.device_name_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox2.ForeColor = System.Drawing.Color.White;
             this.groupBox2.Location = new System.Drawing.Point(14, 11);
@@ -141,6 +144,82 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "CONSOLE";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.freeram_label);
+            this.groupBox4.Controls.Add(this.freeram_progressbar);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox4.ForeColor = System.Drawing.Color.White;
+            this.groupBox4.Location = new System.Drawing.Point(6, 46);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(248, 108);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "HARDWARE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(6, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "RAM :";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.device_name_label);
+            this.panel1.Location = new System.Drawing.Point(52, 17);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 28);
+            this.panel1.TabIndex = 11;
+            // 
+            // freeram_progressbar
+            // 
+            this.freeram_progressbar.Location = new System.Drawing.Point(60, 25);
+            this.freeram_progressbar.Name = "freeram_progressbar";
+            this.freeram_progressbar.Size = new System.Drawing.Size(181, 18);
+            this.freeram_progressbar.TabIndex = 8;
+            // 
+            // freeram_label
+            // 
+            this.freeram_label.AutoSize = true;
+            this.freeram_label.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.freeram_label.ForeColor = System.Drawing.Color.White;
+            this.freeram_label.Location = new System.Drawing.Point(57, 46);
+            this.freeram_label.Name = "freeram_label";
+            this.freeram_label.Size = new System.Drawing.Size(28, 15);
+            this.freeram_label.TabIndex = 9;
+            this.freeram_label.Text = "---";
+            this.freeram_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -153,9 +232,13 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel4.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -168,8 +251,15 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label device_name_label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar freeram_progressbar;
+        private System.Windows.Forms.Label freeram_label;
     }
 }
 
--- adb-fastboot-gui-app/Form1.cs
+++ adb-fastboot-gui-app/Form1.cs
@@ -16,6 +16,8 @@ namespace adb_fastboot_gui_app
 {
     public partial class Form1 : Form
     {
+        public int TotalRam;
+        public float TotalRamf;
         private Process process = new Process();
         private ProcessStartInfo info = new ProcessStartInfo();
         public Form1()
@@ -42,10 +44,11 @@ namespace adb_fastboot_gui_app
                 process.Start();
                 string output = process.StandardOutput.ReadToEnd();
                 string delete = output.Substring(0, output.Length - 34);
-                string raw_device_name = delete.Substring(26);
+                string raw_device_name = delete.Substring(78);
                 string real = raw_device_name.Replace("_", " ");
                 device_name_label.Text = real;
                 textBox1.Text = real;
+                timer1.Start();
             }
             catch (Exception ex)
             {
@@ -53,5 +56,56 @@ namespace adb_fastboot_gui_app
             }
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                info.FileName = "adb.exe";
+                info.Arguments = "shell \"cat /proc/meminfo\"";
+                process.StartInfo = info;
+                process.Start();
+                string str1_totalram = process.StandardOutput.ReadToEnd();
+                string str2_totalram = str1_totalram.Substring(0, str1_totalram.Length - 1049);
+                string str3_totalram = str2_totalram.Substring(17);
+                float int_totalram = float.Parse(str3_totalram);
+                float GB_totalram = int_totalram / (1024 * 1024);
+                TotalRamf = GB_totalram;
+                TotalRam = (int)GB_totalram;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            try
+            {
+                info.FileName = "adb.exe";
+                info.Arguments = "shell \"cat /proc/meminfo\"";
+                process.StartInfo = info;
+                process.Start();
+                string str1_freeram = process.StandardOutput.ReadToEnd();
+                string str2_freeram = str1_freeram.Substring(0, str1_freeram.Length - 992);
+                string str3_freeram = str2_freeram.Substring(75);
+                float float_freeram = float.Parse(str3_freeram);
+                float GB_freeram = float_freeram / (1024 * 1024);
+                float abc = TotalRamf - GB_freeram;
+                freeram_label.Text = abc.ToString("0.00") + "GB" + "" + "/" + "" + TotalRamf.ToString("0.00") + "GB";
+                freeram_progressbar.Maximum = TotalRam;
+                freeram_progressbar.Minimum = 0;
+                freeram_progressbar.Value = TotalRam - (int)GB_freeram;
+
+                textBox1.Text = str1_freeram;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
